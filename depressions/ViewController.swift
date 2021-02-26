5b2f3dcf3a22d3eb2a007bab10bae0b35b667b8d
will <alangprs1@gmail.com>
2021-02-26 17:42:10 +0800

    完成

--- depressions/ViewController.swift
+++ depressions/ViewController.swift
@@ -16,17 +16,16 @@ class ViewController: UIViewController {
     @IBOutlet var answerButton: [UIButton]!
     //答案view
     @IBOutlet var answerView: [UIView]!
+    //分數label
+    @IBOutlet weak var fractionLabel: UILabel!
+    //文章顯示
+    @IBOutlet weak var articleText: UITextView!
     //導入自定義資料
     var topics = [Topics]()
     //題目數
     var index = 0
     //總分
     var sum = 0
-    //四個答案分數
-    var topic1Number = 0
-    var topic2Number = 0
-    var topic3Number = 0
-    var topic4Number = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -50,26 +49,44 @@ class ViewController: UIViewController {
         topics.append(topics8)
         let topics9 = Topics(topicNumber: 9, topics: "有輕生或傷害自己的念頭")
         topics.append(topics9)
-        
+        changAlpha(number: 1, fraction: 0)
         //顯示題號
         numberLabel.text = "\(topics[index].topicNumber)"
         //顯示題目
         topicLabel.text = topics[index].topics
        
     }
+    //分數計算
+    func scoreCalculation(){
+        switch sum {
+        case 0...9:
+            fractionLabel.text = "分數 \n\(sum)分 \n 在正常範圍內"
+        case 10...14:
+            fractionLabel.text = "分數 \n\(sum)分 \n 屬於輕度憂鬱症"
+        case 15...19:
+            fractionLabel.text = "分數 \n\(sum)分 \n 屬於中度憂鬱症"
+        
+        default:
+            fractionLabel.text = "分數 \n\(sum)分 \n 屬於重度憂鬱症"
+        }
+    }
     //按鍵狀態
-    func changAlpha(number:Int){
+    func changAlpha(number:CGFloat,fraction:CGFloat){
         //答案按鍵隱藏
         for i in 0...3{
-            answerButton[i].alpha = CGFloat(number)
-            answerView[i].alpha = CGFloat(number)
+            answerButton[i].alpha = number
+            answerView[i].alpha = number
         }
-        //題目數隱藏
-        topicNumberView.alpha = 0
-        topicLabel.alpha = 0
-        //題目隱藏
-        topicVeiw.alpha = 0
-        topicLabel.alpha = 0
+        //題目數
+        topicNumberView.alpha = number
+        topicLabel.alpha = number
+        //題目
+        topicVeiw.alpha = number
+        topicLabel.alpha = number
+        //結算畫面
+        fractionLabel.alpha = fraction
+        articleText.alpha = fraction
+        articleText.text = "一般來說，較短暫的心情不適或低落，可以主動和朋友或師長聊聊，適當的人際交流可抒發壓力或減輕心理的負擔，也可透過熱愛的事物如運動、聽音樂等方式放鬆自己。\n但若是有下列情況發生，就應該逕行就醫：\n心理或生理的不舒服已經超過自身所能控制的範圍。心情持續低落，導致家庭、工作、學業的表現大受影響，時間超過兩個月。出現自殺的傾向或念頭。治療憂鬱症在生物學的最終目標是改善大腦調節情緒的能力，其基本的治療方法是藥物治療，如常見的抗憂鬱劑等等，主要作用為增加神經傳導物質的濃度，約50% -75%憂鬱症的病人對抗鬱劑有好的反應，服藥時間約3-4週才有明顯成效，通常需持續服用至少3-6個月。心理治療、電療也會和藥物治療並用，心理治療期望改善患者的自我認知的行為或提高病患在人際互動中的信任感，並增進病患忍受情緒變化的能力；電療的方式，是一種針對特別情況的非必要方法，如情況較為嚴重或藥物無法控制時，主要幫助神經傳導物質正常運作、穩定患者的情緒。\n尋求幫助以下為心理諮詢的電話、網路資源，也可以上網搜尋各地的衛生、醫療機構。電話：台北市撥打1999市民專線轉2521（直撥1980）生命線（直撥1995）衛生署安心專線（0800-788-995）24hr全國照顧者免付費關懷專線（0800-507272）\n內政部男性關懷專線（0800-013-999）\n華人心理治療基金會(以評估狀況為主)https://www.tip.org.tw/therapyonline\n張老師全球資訊網http://www.1980.org.tw/web3-20101110/main.php?customerid=3"
     }
     //按下答案button之後動作
     func chooseAnswer (){
@@ -82,7 +99,9 @@ class ViewController: UIViewController {
             //顯示題目
             topicLabel.text = topics[index].topics
             }else if index == 8{
-                changAlpha(number: 0)
+                changAlpha(number: 0, fraction: 1)
+                scoreCalculation()//分數計算
+               
             }
     }
     //完全沒有
